286575316fbc4f14640a1d6ad3dd27fe6ac6a26a
zLeggi <leandrosalmeida200021@gmail.com>
2022-08-24 12:20:00 -0300

    Primeira versão pronta

---
+++ projetos/Calculadora/script.js
@@ -0,0 +1,23 @@
+function inserir(num) {
+  var numero = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function limpar(){
+  document.getElementById('resultado').innerHTML = "";
+}
+
+function apagar(){
+  var resultado = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function caucular(){
+  var resultado = document.getElementById('resultado').innerHTML;
+  
+  if(resultado) {
+    document.getElementById('resultado').innerHTML = eval(resultado);
+  } else {
+    document.getElementById('resultado').innerHTML = "???"
+  }
+}
